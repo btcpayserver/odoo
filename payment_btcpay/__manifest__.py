1421168374ca57728cdf87402da4156461daf47b
ndeet <andy.tasch@gmail.com>
2024-07-31 14:22:32 +0200

    Bumping version.

--- payment_btcpay/__manifest__.py
+++ payment_btcpay/__manifest__.py
@@ -25,7 +25,7 @@
     'author': 'Vandekul',
     'website': 'https://github.com/btcpayserver/odoo',
     'category': 'Accounting/Payment Providers',
-    'version': '16.0.1.0',
+    'version': '16.0.1.1',
     'license': 'GPL-3',
     'application': False,
     'installable': True,
